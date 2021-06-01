3a2d9dfc5923f3f1be7d0565fa17940f1a745282
Michael <michael.g186@live.de>
2021-06-01 16:34:45 +0200

    Added patch for GenerateAuction. Auctions will now have a duration of 0 days.

---
+++ ROIData/Patching/AuctionDefinitionGenerateAuctionPatcher.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using ProjectAutomata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROIData.Patching {
+    [HarmonyPatch(typeof(AuctionDefinition))]
+    [HarmonyPatch("GenerateAuction")]
+    [HarmonyPatch(new Type[] { typeof(IActor), typeof(IAuctionReward) })]
+    class AuctionDefinitionGenerateAuctionPatcher {
+        static void Postfix(AuctionDefinition __instance, ref Auction __result, ref IActor auctioner, ref IAuctionReward reward) {
+            __result = new Auction(__instance, 0, __instance.startingBid, __instance.bidController, reward, auctioner);
+        }
+    }
+}
