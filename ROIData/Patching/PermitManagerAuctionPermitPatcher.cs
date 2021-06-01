3868e3cfb4c63ad798320c4c85077fd8388717db
Michael <michael.g186@live.de>
2021-06-01 17:14:08 +0200

    Auctions will now end instantly.

---
+++ ROIData/Patching/PermitManagerAuctionPermitPatcher.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+using ProjectAutomata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROIData.Patching {
+    [HarmonyPatch(typeof(PermitManager))]
+    [HarmonyPatch("AuctionPermit")]
+    [HarmonyPatch(new Type[] { typeof(IActor), typeof(Region), typeof(PermitType) })]
+    class PermitManagerAuctionPermitPatcher {
+        static void Postfix(PermitManager __instance, ref IActor actor, ref Region region, ref PermitType permitType, ref bool __result) {
+            __instance.GrantPermitToActor(region, permitType, actor);
+            ManagerBehaviour<AuctionsManager>.instance.EndCurrentAuction(true);
+            __result = true;
+        }
+    }
+}
